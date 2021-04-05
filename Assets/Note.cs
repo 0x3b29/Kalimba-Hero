1d941a803c4e8767a0b57815b70acd5910e0a934
oli4@home <wagener_olivier@hotmail.com>
2021-04-05 22:01:33 +0200

    save good first project state

---
+++ Assets/Note.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class RootObject
+{
+    public List<Note> notes;
+}
+
+[Serializable]
+public class Note
+{
+    public String caption;
+    public int lowerBound;
+    public int upperBound;
+    public float thresholdValue;
+
+    public bool triggered { get; set; }
+
+    private float maxValue { get; set; }
+    private float oldValue { get; set; }
+
+    [field: NonSerialized] private GameObject thresholdSliderParent { get; set; }
+    [field: NonSerialized] private Slider thresholdSlider { get; set; }
+    [field: NonSerialized] private Image thresholdBackgroundPanelImage { get; set; }
+
+    public Note(string caption, int lowerBound, int upperBound, float thresholdValue)
+    {
+        this.caption = caption;
+        this.lowerBound = lowerBound;
+        this.upperBound = upperBound;
+        this.thresholdValue = thresholdValue;
+        this.maxValue = thresholdValue * 1.5f;
+    }
+
+    public void CreateThresholdSlider(GameObject thresholdSliderParent)
+    {
+        this.thresholdSliderParent = thresholdSliderParent;
+
+        thresholdSliderParent.name = caption + " " + " Threshold Slider";
+
+        SetThresholdSliderParentPosition();
+
+        thresholdSlider = thresholdSliderParent.GetComponentInChildren<Slider>();
+        thresholdBackgroundPanelImage = thresholdSliderParent.transform.GetChild(0).transform.GetComponent<Image>();
+
+        this.maxValue = thresholdValue * 1.5f;
+        thresholdSlider.maxValue = thresholdValue * 1.5f;
+        thresholdSlider.value = thresholdValue;
+
+        thresholdSlider.onValueChanged.AddListener(delegate {
+            TresholdSliderValueChanged(thresholdSlider);
+        });
+    }
+
+    public void TresholdSliderValueChanged(Slider thresholdSlider)
+    {
+        thresholdValue = thresholdSlider.value;
+    }
+
+    private void SetThresholdSliderParentPosition()
+    {
+        Vector3 localPosition = thresholdSliderParent.GetComponent<RectTransform>().anchoredPosition;
+        localPosition.x = (lowerBound + ((upperBound - lowerBound) / 2)) / 512f * 1000;
+        thresholdSliderParent.GetComponent<RectTransform>().anchoredPosition = localPosition;
+    }
+
+    public void SetLowerBound(int newLowerBound)
+    {
+        if (newLowerBound > upperBound)
+        {
+            lowerBound = upperBound;
+        }
+        else
+        {
+            lowerBound = newLowerBound;
+        }
+
+        SetThresholdSliderParentPosition();
+    }
+
+    public void SetUpperBound(int newUpperBound)
+    {
+        if (newUpperBound < lowerBound)
+        {
+            upperBound = lowerBound;
+        }
+        else
+        {
+            upperBound = newUpperBound;
+        }
+
+        SetThresholdSliderParentPosition();
+    }
+
+    public void SetValue(float value)
+    {
+        if (value > maxValue)
+        {
+            maxValue = value;
+            thresholdSlider.maxValue = value;
+        }
+
+        thresholdBackgroundPanelImage.fillAmount = 1 / maxValue * value;
+
+        if (triggered)
+        {
+            if (value <= thresholdValue)
+            {
+                triggered = false;
+            }
+        }
+        else
+        {
+            if (value > oldValue && value > thresholdValue)
+            {
+                Debug.Log(caption + " got triggered."); // by " + value + " which was more than " + thresholdValue);
+                triggered = true;
+            }
+        }
+
+        oldValue = value;
+    }
+
+    public int GetLowerBound() { return lowerBound; }
+    public int GetUpperBound() { return upperBound; }
+}
+
---
+++ Assets/SoundAnalyzer.cs
@@ -0,0 +1,323 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundAnalyzer : MonoBehaviour
+{
+    public int qSamples = 8192; //1024;
+    public int binSize = 8192; // you can change this up, I originally used 8192 for better resolution, but I stuck with 1024 because it was slow-performing on the phone
+
+    float[] spectrum;
+    int samplerate;
+
+    public Text display; // drag a Text object here to display values
+    public bool mute;
+    public AudioMixer masterMixer; // drag an Audio Mixer here in the inspector
+
+    int textWidth = 512;
+    int textHeight = 200;
+    Texture2D texture2d;
+
+    Note selectedNote;
+    List<Note> notes = new List<Note>();
+
+    public Dropdown noteSelectorDropdown;
+    public Slider upperBoundSlider;
+    public Slider lowerBoundSlider;
+
+    public Button saveButton;
+    public Button loadButton;
+    public Button addButton;
+    public Button clearButton;
+
+    public Button removeButton;
+    public Button unselectButton;
+
+    public Button nextButton;
+    public Button previousButton;
+
+    public InputField noteNameInput;
+
+    public GameObject thresholdSliderPrefab;
+    public GameObject thresholdSliderParent;
+
+    void Start()
+    {
+        spectrum = new float[binSize];
+        samplerate = AudioSettings.outputSampleRate;
+
+        // starts the Microphone and attaches it to the AudioSource
+        GetComponent<AudioSource>().clip = Microphone.Start(null, true, 10, samplerate);
+        GetComponent<AudioSource>().loop = true; // Set the AudioClip to loop
+        while (!(Microphone.GetPosition(null) > 0)) { } // Wait until the recording has started
+        GetComponent<AudioSource>().Play();
+
+        // Mutes the mixer. You have to expose the Volume element of your mixer for this to work. I named mine "masterVolume".
+        masterMixer.SetFloat("masterVolume", -80f);
+
+        texture2d = new Texture2D(textWidth, textHeight);
+
+        for (int x = 0; x < textWidth; x++)
+        {
+            for (int y = 0; y < textHeight; y++)
+            {
+                texture2d.SetPixel(x, y, Color.black);
+            }
+        }
+
+        texture2d.Apply();
+
+        GameObject.Find("Image").GetComponent<RawImage>().texture = texture2d;
+
+        updateDropdownOptions();
+        DropdownValueChanged(noteSelectorDropdown);
+
+        noteSelectorDropdown.onValueChanged.AddListener(delegate 
+        { DropdownValueChanged(noteSelectorDropdown); });
+
+        upperBoundSlider.onValueChanged.AddListener(delegate 
+        { UpperBoundSliderValueChanged(upperBoundSlider); });
+
+        lowerBoundSlider.onValueChanged.AddListener(delegate 
+        { LowerBoundSliderValueChanged(lowerBoundSlider); });
+
+        saveButton.onClick.AddListener(delegate
+        { SaveButtonClick(saveButton); });
+
+        loadButton.onClick.AddListener(delegate
+        { LoadButtonClick(loadButton); });
+
+        clearButton.onClick.AddListener(delegate
+        { ClearButtonClick(clearButton); });
+
+        addButton.onClick.AddListener(delegate
+        { AddButtonClick(addButton); });
+
+        nextButton.onClick.AddListener(delegate
+        { NextButtonClick(nextButton); });
+
+        previousButton.onClick.AddListener(delegate
+        { PreviousButtonClick(previousButton); });
+
+        unselectButton.onClick.AddListener(delegate
+        { UnselectButtonClick(unselectButton); });
+
+        DropdownValueChanged(noteSelectorDropdown);
+    }
+
+    void SaveButtonClick(Button saveButton)
+    {
+        SaveData();
+    }
+
+    void LoadButtonClick(Button loadButton)
+    {
+        LoadData();
+        updateDropdownOptions();
+        noteSelectorDropdown.value = notes.FindIndex(x => x == selectedNote);
+    }
+
+    void ClearButtonClick(Button clearButton)
+    {
+        notes.Clear();
+        foreach(Transform child in thresholdSliderParent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        noteSelectorDropdown.options.Clear();
+    }
+
+    void NextButtonClick(Button nextButton)
+    {
+        if (selectedNote != null)
+            Debug.Log("Selected was " + selectedNote.caption);
+        else
+            Debug.Log("Selected was null");
+
+        if (selectedNote == null)
+        {
+            selectedNote = notes[0];
+        }
+        else if (selectedNote == notes[notes.Count - 1])
+        {
+            selectedNote = notes[0];
+        }
+        else
+        {
+            selectedNote = notes[notes.FindIndex(x => x == selectedNote) + 1];
+        }
+
+        noteSelectorDropdown.value = notes.FindIndex(x => x == selectedNote);
+        // DropdownValueChanged(noteSelectorDropdown);
+        Debug.Log("Selected is " + selectedNote.caption);
+    }
+
+    void PreviousButtonClick(Button previousButton)
+    {
+        if (selectedNote != null)
+            Debug.Log("Selected was " + selectedNote.caption);
+        else
+            Debug.Log("Selected was null");
+
+        if (selectedNote == null)
+        {
+            selectedNote = notes[notes.Count - 1];
+        }
+        else if (selectedNote == notes[0])
+        {
+            selectedNote = notes[notes.Count - 1];
+        }
+        else
+        {
+            selectedNote = notes[notes.FindIndex(x => x == selectedNote) - 1];
+        }
+
+        noteSelectorDropdown.value = notes.FindIndex(x => x == selectedNote);
+        Debug.Log("Selected is " + selectedNote.caption);
+    }
+
+    void UnselectButtonClick(Button unselectButton)
+    {
+        selectedNote = null;
+    }
+
+    void AddButtonClick(Button addButton)
+    {
+        Note newNote = new Note(noteNameInput.text, 0, 0, 0);
+        newNote.CreateThresholdSlider(Instantiate(thresholdSliderPrefab, thresholdSliderParent.transform));
+        notes.Add(newNote);
+
+        updateDropdownOptions();
+        DropdownValueChanged(noteSelectorDropdown);
+    }
+
+    void RemoveButtonClick(Button removeButton)
+    {
+        notes.Remove(selectedNote);
+        updateDropdownOptions();
+        DropdownValueChanged(noteSelectorDropdown);
+    }
+
+    void updateDropdownOptions()
+    {
+        noteSelectorDropdown.options.Clear();
+
+        foreach (Note note in notes)
+        {
+            noteSelectorDropdown.options.Add(new Dropdown.OptionData(note.caption));
+        }
+    }
+
+    void DropdownValueChanged(Dropdown change)
+    {
+        if (change.options.Count == 0)
+        {
+            return;
+        }
+
+        Debug.Log("New Value : " + change.value);
+        selectedNote = notes[change.value];
+
+        lowerBoundSlider.value = selectedNote.GetLowerBound();
+        upperBoundSlider.value = selectedNote.GetUpperBound();
+    }
+
+    void UpperBoundSliderValueChanged(Slider upperBoundSlider)
+    {
+        if (selectedNote != null)
+            selectedNote.SetUpperBound(Mathf.RoundToInt(upperBoundSlider.value));
+    }
+
+    void LowerBoundSliderValueChanged(Slider lowerBoundSlider)
+    {
+        if (selectedNote != null)
+            selectedNote.SetLowerBound(Mathf.RoundToInt(lowerBoundSlider.value));
+    }
+
+    void Update()
+    {
+        for (int x = 0; x < textWidth; x++)
+        {
+            for (int y = textHeight; y > 0; y--)
+            {
+                texture2d.SetPixel(x, y + 1, texture2d.GetPixel(x, y));
+            }
+        }
+
+        GetComponent<AudioSource>().GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        foreach(Note note in notes)
+        {
+            float accumulator = 0;
+
+            for (int i = note.GetLowerBound(); i < note.GetUpperBound(); i++)
+            {
+                accumulator += spectrum[i];
+            }
+
+            note.SetValue(accumulator);
+        }
+
+        for (int i = 0; i < binSize; i++)
+        {
+            if (i < textWidth)
+            {
+                texture2d.SetPixel(i, 0, new Color(MapRange(spectrum[i], 0, 0.01f, 0, 1), 0, 0));
+
+                foreach (Note note in notes)
+                {
+                    if (i == note.GetLowerBound() || i == note.GetUpperBound())
+                    {
+
+                        if (note.triggered)
+                        {
+                            texture2d.SetPixel(i, 0, Color.yellow);
+                        }
+                        else if (note == selectedNote)
+                        {
+                            texture2d.SetPixel(i, 0, Color.green);
+                        }
+                        else
+                        {
+                            texture2d.SetPixel(i, 0, Color.blue);
+                        }
+                    }
+                }
+            }
+        }
+
+        texture2d.Apply();
+        GameObject.Find("Image").GetComponent<RawImage>().texture = texture2d;
+    }
+
+    public void SaveData()
+    {
+        RootObject rootObject = new RootObject();
+        rootObject.notes = notes;
+
+        Debug.Log(JsonUtility.ToJson(rootObject));
+    }
+
+    public void LoadData()
+    {
+        String kalimbaSetup = "{\"notes\":[{\"caption\":\"C\",\"lowerBound\":85,\"upperBound\":93,\"thresholdValue\":0.03794857859611511},{\"caption\":\"D\",\"lowerBound\":95,\"upperBound\":105,\"thresholdValue\":0.044449105858802798},{\"caption\":\"E\",\"lowerBound\":107,\"upperBound\":117,\"thresholdValue\":0.009955160319805146},{\"caption\":\"F\",\"lowerBound\":118,\"upperBound\":124,\"thresholdValue\":0.01148715615272522},{\"caption\":\"G\",\"lowerBound\":128,\"upperBound\":139,\"thresholdValue\":0.013032621704041958},{\"caption\":\"A\",\"lowerBound\":146,\"upperBound\":156,\"thresholdValue\":0.02550424449145794},{\"caption\":\"B\",\"lowerBound\":164,\"upperBound\":174,\"thresholdValue\":0.0694345161318779},{\"caption\":\"C*\",\"lowerBound\":176,\"upperBound\":185,\"thresholdValue\":0.1029769703745842},{\"caption\":\"D*\",\"lowerBound\":196,\"upperBound\":206,\"thresholdValue\":0.034559790045022967},{\"caption\":\"E*\",\"lowerBound\":222,\"upperBound\":231,\"thresholdValue\":0.09041664004325867},{\"caption\":\"F*\",\"lowerBound\":233,\"upperBound\":243,\"thresholdValue\":0.02607892081141472},{\"caption\":\"G*\",\"lowerBound\":264,\"upperBound\":275,\"thresholdValue\":0.10083159804344177},{\"caption\":\"A*\",\"lowerBound\":295,\"upperBound\":309,\"thresholdValue\":0.013621526770293713},{\"caption\":\"B*\",\"lowerBound\":331,\"upperBound\":342,\"thresholdValue\":0.009923440404236317},{\"caption\":\"C**\",\"lowerBound\":353,\"upperBound\":365,\"thresholdValue\":0.015800992026925088},{\"caption\":\"D**\",\"lowerBound\":392,\"upperBound\":404,\"thresholdValue\":0.02569706365466118},{\"caption\":\"E**\",\"lowerBound\":451,\"upperBound\":469,\"thresholdValue\":0.039687380194664}]}";
+
+        RootObject rootObject = JsonUtility.FromJson<RootObject>(kalimbaSetup);
+        notes = rootObject.notes;
+
+        foreach (Note note in notes)
+        {
+            note.CreateThresholdSlider(Instantiate(thresholdSliderPrefab, thresholdSliderParent.transform));
+        }
+    }
+
+    public static float MapRange(float value, float inputRangeFrom, float inputRangeTo, float outputRangeFrom, float outputRangeTo)
+    {
+        return (value - inputRangeFrom) * (outputRangeTo - outputRangeFrom) / (inputRangeTo - inputRangeFrom) + outputRangeFrom;
+    }
+}
